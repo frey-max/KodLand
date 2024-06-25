ce61531dc76082d8e3309307a19c83f41c50ae38
frey-max <129594955+frey-max@users.noreply.github.com>
2024-06-25 21:51:43 +0400

    M1Y4.py

--- M1Y1.py
+++ M1Y1.py
@@ -1,14 +1,99 @@
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное",
-            "БРО": "Брат, друг",
-            "Я ПОНЯЛА ТЕБЯ БРО": "мне все понятно, хороший человек"
-            }
-            
-while True: 
-    word = input("Введите непонятное слово (большими буквами!): ")
+import discord
+from discord.ext import commands
+import random
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-    if word in meme_dict.keys():
-        print(meme_dict[word])
-    else:
-        print("Ваш сын придумал слово!")
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+    
+
+
+    
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Привет, {bot.user}")
+
+
+
+
+@bot.command()
+async def bye(ctx):
+    await ctx.send(f"Покедова")
+
+
+
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+
+
+
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+
+
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+
+
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+
+
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+
+
+@cool.command(name='bot')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Yes, the bot is cool.')
+
+
+bot.run("token")
